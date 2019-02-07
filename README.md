d39cb70a89724e89084e67ba36a1cd150db8b6c6
José Antonio Reyes <imjaroiswebdev@gmail.com>
2019-02-07 06:33:04 -0300

    Add test convention for Red, Green Refactor flow
    Also adds Coverage report screen capture to README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Boilerplate for Testing NodeJs Applications with Jest and Generating Coverage Report
+
+## Coverage Report for Basic Implementation
+
+![Coverage Report](./coverage_report.png)
---
+++ src/sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum')
+
+test('Given 1 and 2, should sums 3', () => {
+  const given = {
+    a: 1,
+    b: 2
+  }
+
+  const expected = 3
+
+  const actual = sum(given.a, given.b)
+
+  expect(actual).toEqual(expected)
+})
